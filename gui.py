f223e9e84d48efd84daf4ff7376a303f71fda9e1
Toni Kucic <tkucic@gmail.com>
2020-12-31 14:33:18 +0200

    Configure colors and UI for the gui

--- gui.py
+++ gui.py
@@ -1,13 +1,19 @@
 import tkinter, os
 from tkinter import ttk
 from client import reader
+BG ='#2C2C2C'
+FG ='gray'
 
 class readerGui:
     def __init__(self, master):
 
         self.master = master
+        master.withdraw()
+        #master.overrideredirect(True)
         master.title('Modbus TCP client')
         master.geometry('600x600')
+        master.minsize(width=600, height=600)
+        master.configure(bg=BG)
 
         #Configure rows and columns 7x4 geometry
         tkinter.Grid.rowconfigure(self.master, 0, weight=1) #Content expands on all sides
@@ -34,41 +40,73 @@ class readerGui:
         self.regDataListBox.heading('Register', text='Register', anchor='center')
         self.regDataListBox.heading('Value', text='Value', anchor='center')
         self.regDataListBox.heading('Description', text='Description', anchor='w')
-
+        self.regDataListBox.state(('disabled',))
         self.regDataListBox.grid(row=0, column=0, sticky="NSWE", padx=5, pady=5)
         self.scrollbar.config(command = self.regDataListBox.yview)
 
         #Command layers
         cmdFrame = ttk.Frame(self.master)
-        cmdFrame.grid(row=1, column=0, columnspan=2, sticky='WE')
+        cmdFrame.grid(row=1, column=0, columnspan=2, sticky='W', padx=5, pady=2)
         for i in range(0,5):
             tkinter.Grid.rowconfigure(cmdFrame, i, weight=1)
         for i in range(0,6):
             tkinter.Grid.columnconfigure(cmdFrame, i, weight=1)
         for i, text in enumerate(['Register type', 'Register', 'Value', 'Increment', 'Send once', 'Cycle send']):
-            infoLabel = ttk.Label(cmdFrame, text=text)
-            infoLabel.grid(row=0, column=i, sticky="WE")
+            infoLabel = ttk.Label(cmdFrame, text=text, anchor='center',relief=tkinter.RIDGE)
+            infoLabel.grid(row=0, column=i, sticky="WE", pady=5)
 
-        self.cmd_regType = [tkinter.StringVar(value='Coil') for i in range(4)]
         self.cmd_reg = [tkinter.StringVar(value='10000') for i in range(4)]
         self.cmd_value = [tkinter.StringVar(value='0') for i in range(4)]
         self.cmd_inc = [tkinter.IntVar() for i in range(4)]
         self.cmd_once = [tkinter.IntVar() for i in range(4)]
         self.cmd_cyclic = [tkinter.IntVar() for i in range(4)]
-        cmd_dropdown = [ttk.OptionMenu(cmdFrame, self.cmd_regType[i], "Coil", "Holding register") for i in range(4)]
+        self.cmd_regType = [ttk.Combobox(cmdFrame, state="readonly", values=["           Coil           ", "Holding register"]) for i in range(4)]
         for i in range(4):
-            cmd_dropdown[i].grid(row=i+1, column=0, sticky='WE')
-            #cmd_dropdown[i]['menu'].config(font=FONT, bg=BG2, fg=TEXT_COLOR)
-            #cmd_dropdown[i].config(font=FONT, bg=BG2, fg=TEXT_COLOR)
-        cmd_regEntry = [ttk.Entry(cmdFrame, textvariable=self.cmd_reg[i]).grid(row=i+1, column=1, sticky='WE') for i in range(4)]
+            self.cmd_regType[i].grid(row=i+1, column=0, sticky='WE')
+            self.cmd_regType[i].current(0)
+        cmd_regEntry = [ttk.Entry(cmdFrame, textvariable=self.cmd_reg[i]).grid(row=i+1, column=1, sticky='W') for i in range(4)]
         cmd_regValueEntry = [ttk.Entry(cmdFrame, textvariable=self.cmd_value[i]).grid(row=i+1, column=2, sticky='WE') for i in range(4)]
-        cmd_incCb = [ttk.Checkbutton(cmdFrame, variable=self.cmd_inc[i]).grid(row=i+1, column=3, sticky='WE') for i in range(4)]
-        cmd_sendOnceCb = [ttk.Checkbutton(cmdFrame, variable=self.cmd_once[i]).grid(row=i+1, column=4, sticky='WE') for i in range(4)]
-        cmd_cyclicCb = [ttk.Checkbutton(cmdFrame, variable=self.cmd_cyclic[i]).grid(row=i+1, column=5, sticky='WE') for i in range(4)]
+        cmd_incCb = [ttk.Checkbutton(cmdFrame, variable=self.cmd_inc[i]).grid(row=i+1, column=3, sticky='WE', padx=25) for i in range(4)]
+        cmd_sendOnceCb = [ttk.Checkbutton(cmdFrame, variable=self.cmd_once[i]).grid(row=i+1, column=4, sticky='WE', padx=25) for i in range(4)]
+        cmd_cyclicCb = [ttk.Checkbutton(cmdFrame, variable=self.cmd_cyclic[i]).grid(row=i+1, column=5, sticky='WE', padx=25) for i in range(4)]
 
         self.statusTxt = tkinter.StringVar(value='IP Address: Not connected')
-        statusBar = ttk.Label(self.master, anchor='w', textvariable=self.statusTxt)
+        statusBar = ttk.Label(self.master, anchor='w', textvariable=self.statusTxt,relief=tkinter.RIDGE)
         statusBar.grid(row=2, column=0, columnspan=2, sticky="SWE")
+
+        #Configure styles
+        style = ttk.Style()
+        style.theme_use('default')
+        style.configure('.', font=('Verdana', 8), foreground=FG, background=BG)
+        style.configure('Treeview',
+            background=BG,
+            foreground=FG,
+            fieldbackground=BG,
+            rowheight=15)
+        style.configure('Treeview.Heading',
+            fieldbackground=BG,
+            rowheight=20,
+            relief="ridge")
+        style.map('Treeview',
+            background=[('selected', '#1A4EA8')],
+            foreground=[('selected', 'white')])
+        style.map('Treeview.Heading',
+            background=[('active', BG)],
+            foreground=[('active', FG)])
+        style.configure('TEntry', foreground='black', fieldbackground=FG)
+        style.map('TEntry',
+            selectbackground=[('focus', '#1A4EA8'), ('!focus', '#1A4EA8')])
+        style.map('TCombobox',
+            background=[('readonly', BG)],
+            foreground=[('readonly', 'black')],
+            selectbackground=[('readonly', '#1A4EA8')],
+            fieldbackground=[('readonly', FG)])
+        style.map('TCheckbutton',
+            background=[('active', BG)],
+            foreground=[('selected', '#1A4EA8')],
+            selectcolor=[('selected', '#1A4EA8')])
+        
+        master.deiconify()
 
     def createClient(self, xmlFile, cycleTime_ms=500):
         #Create client and commence updating and sending commands on provided cycle time
@@ -109,7 +147,7 @@ class readerGui:
                             self.cmd_value[i].set(str(value + 1))
                         #Write the actual register
                         value = int(self.cmd_value[i].get())
-                        if self.cmd_regType[i].get() == 'Coil':
+                        if 'Coil' in self.cmd_regType[i].get():
                             #Coils can take only 1 or 0
                             if value < 0:
                                 value = 0
@@ -118,13 +156,21 @@ class readerGui:
                                     value = 1
                                 else:
                                     value = 0
-                            self.client.write_coil(register=int(self.cmd_reg[i].get()), value=value)
+                            response = self.client.write_coil(register=int(self.cmd_reg[i].get()), value=value)
+                            
                         else:
-                            self.client.write_register(register=int(self.cmd_reg[i].get()), value=value)
+                            response = self.client.write_register(register=int(self.cmd_reg[i].get()), value=value)
+                        #If response was exception
+                        if 'Exception' in str(response):
+                            self.cmd_once[i].set(0)
+                            self.cmd_cyclic[i].set(0)
+                            self.cmd_value[i].set(response)
+
                 except Exception as e:
                     self.cmd_once[i].set(0)
                     self.cmd_cyclic[i].set(0)
                     self.cmd_value[i].set(e)
+
         self.master.after(500, self._sendCmds)
 
     def _update(self):
